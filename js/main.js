b41e2bd854ce8e933ef522d3c7d816bb97fe0f44
great-hunter <greathunter@mail.ru>
2021-04-16 16:30:35 +0300

    create: form validates and fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+
+
+  //Слайдеры
+
   const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
     loop: true,
@@ -20,10 +24,16 @@ $(document).ready(function () {
     },
   });
 
+
+  //Мобильное меню
+
   var menuButton = $(".menu-button");
   menuButton.on('click', function () {
     $(".navbar-bottom").toggleClass('navbar-bottom--visible');
   });
+
+
+  //Параллакс
 
   $('.parallax-window').parallax({
     imageSrc: 'img/newsletter-bg.jpg',
@@ -31,16 +41,17 @@ $(document).ready(function () {
   });
 
 
+  //Модальное окно
 
   var modalButton = $('[data-togle="modal"]');
   var closeModalButton = $('.modal__close');
   modalButton.on('click', openModal);
   closeModalButton.on('click', closeModal);
   $(document).keyup(function(evet) {
-	if (evet.key === "Escape" || evet.keyCode === 27) {
-		closeModal(evet);
-	}
-});
+    if (evet.key === "Escape" || evet.keyCode === 27) {
+      closeModal(evet);
+    }
+  });
 
   function openModal() {
     var modalOverlay = $('.modal__overlay');
@@ -56,5 +67,32 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+
+
+  //Обработка форм
+
+  $('.form').each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "At least 2 characters required",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Phone number required",
+        },
+      },
+    });
+  });
+
+
+  //Обработка форм
+
+  $('.input-phone').mask('+7 (000) 000-00-00');
 
 });
